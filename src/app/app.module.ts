bff8326635e3a8582786c1cdca19b3c04cc4bbb9
emilyzimmerman <zimmermanemily121@gmail.com>
2023-03-31 23:14:00 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
